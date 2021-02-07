d6475f43f0a7400663c3a4f53dc5623268cff4f2
Felipe Santa Cruz Simão <felipesantacruz6@gmail.com>
2021-02-07 14:41:04 -0300

    up atualização

---
+++ projeto-react-felipe/backend/config/database.js
@@ -0,0 +1,11 @@
+const db = require('mysql');// pede agua
+
+const Connection = db.createConnection({ // busca agua pra de cima beber
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database:'fseletro'
+    });
+
+
+module.exports = Connection;
